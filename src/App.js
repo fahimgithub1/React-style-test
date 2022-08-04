a2045aa9f429271946e273f6c3c631897d7a1e59
unknown <fahimjoy768@gmial.com>
2022-08-05 01:59:56 +0600

    set daynamic style from folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import './styles_files/normal.css';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [inputValu, setInputValu] = useState('');
+  const [valid, setValid] = useState(true);
+
+  const inputChanger = event =>{
+    setInputValu(event.terget.value);
+    console.log(event.terget.value);
+  };
+
+  const submitHandel = () =>{
+    if(inputValu.trim().length === 0){
+      setValid(false);
+      return;
+    }
+  }
+
   return (
     <div className="App">
       <div className="box-1">
         <label className='label'>Input data</label>
-        <input type='text'></ input> <br/>
-        <button>Set Values</button>
+        <input type="text" 
+               className={`input ${!valid? 'inValid':''}`}
+               onChange={inputChanger}
+        />
+        <br/>
+        <button onClick={submitHandel}>Set Values</button>
       </div>
 
       <div className="box-2">
         <p>Frist inputed data</p>
       </div>
-      
       <div className="box-2">
         <p>Frist inputed data</p>
       </div>
